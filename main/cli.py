5ac675f7c97f86e087cc26928245ec0d8434cb24
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-09-06 22:37:04 +0300

    added game update command to cli

--- main/cli.py
+++ main/cli.py
@@ -3,7 +3,7 @@
 import click
 
 from db import session
-from models import Game, Customer, Order
+from models import Game, Customer, Order, update
 
 @click.group()
 def cli():
@@ -34,7 +34,7 @@ def view_customer_list(lists):
     get_orders_list()   
     
 @cli.command()
-@click.option('-c', '--customer', prompt="Enter name, email", help="Enter your name, email")
+@click.option('-c', '--customer', prompt="Enter name, email", help="Enter your 'name, email'")
 def add_customer(customer):
     name, email = customer.split(',')
     existing_customer = session.query(Customer).filter_by(email= email).first()
@@ -44,7 +44,9 @@ def add_customer(customer):
     new_customer = Customer(name, email)
     session.add(new_customer)
     session.commit()  
-    click.echo(f'Added new customer: {name} ({email}) successfully')       
+    click.echo(f'Added new customer: {name} ({email}) successfully')   
+    
+    # update_game(customer_id)    
         
 @cli.command()
 @click.option('-g', '--game', nargs=4, type=click.Tuple([str, str, str, int]), prompt="Enter title, genre, platform, price", help="Enter title, genre, platform, price")                      
@@ -54,9 +56,23 @@ def add_game(game):
     session.add(new_game)
     session.commit()
     click.echo(f'Game Title: {title} Genre: {genre} Platform: {platform} Price: {price} has been added successfully')
+
+@cli.command()
+# @click.option('-u', '--updates',prompt="Update game", help="Update Game")
+def update_game():
+    click.echo(f'\n Fetching Games list \n')
+    get_games_list()
     
-
-
+    id = input("\n Enter game id to update: \n")
+    game_update(id)
+    
+    
+@cli.command()
+@click.option('-o', '--order', prompt="Add new Order", help="Order Transaction: quantity, customer_id, game_id")
+def new_order(order):
+    quantity, customer_id, game_id = order.split(',')
+    new_order = Order(quantity, customer_id)
+    
  
 def get_customer_list():
     customer_list = session.query(Customer).all()
@@ -80,7 +96,19 @@ def get_orders_list():
         results.append(f'{order.id}  Quantity: {order.quantity}  Game-id: {order.game_id}  Customer-id: {order.customer_id}  Total_price: {order.total_price}') 
     click.echo(f'\n'.join(results))          
            
-
+def game_update(id):
+    updates = session.query(Game).filter_by(id=id).first()
+    print(f' \n id: {updates.id} Title: {updates.title}  Genre: {updates.genre}  PLatform: {updates.platform}  Price: {updates.price} \n')
+    # return update
+    
+    changes = input("\n Enter the updates you'd like to make: (title, genre, platform, price)\n")
+    title, genre, platform, price = changes.split(",")
+   
+    session.query(Game).filter_by(id=id).update({"title":title, "genre":genre, "platform":platform, "price":price})
+    session.commit()
+    get_games_list()
+    
+    
     
 
 
